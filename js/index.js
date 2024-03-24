5171480a6c9c18bb3a352303338be007ff20e82a
PeaAni <161184355+PeaAni@users.noreply.github.com>
2024-03-24 14:59:31 +0700

    make cart

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // alert("เว็บไซต์นี้ทำเพื่อจุดประสงค์ทางการศึกษาเท่านั้น")
+
+const counterSpan = document.getElementById("NumberCart");
+let count = 0;
+
+const buttons = document.querySelectorAll(".cart-button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    count++;
+    counterSpan.textContent = count;
+  });
+});
